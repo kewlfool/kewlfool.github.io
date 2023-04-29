528a0059aa85b6598a0c151a43d1e7967a20a716
manbir87 <38939907+kewlfool@users.noreply.github.com>
2023-04-29 13:07:19 -0300

    new sketch

--- sketch.js
+++ sketch.js
@@ -1,57 +1,64 @@
-var img;
-var smallPoint, largePoint;
 
-var colors = [];
-var index = 0;
 
-var angle = 0;
+let pos;
 
-// function preload() {
-//   img = loadImage("../images/bg.jpg");
 
-// }
-var alph = 10;
+let walks = [];
 
 
 function windowResized() {
-	resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function setup() {
-  var canvas = createCanvas(windowWidth, windowHeight);
+  
+   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.id('sketch-container'); 
-  colors.push(color(255, 200, 0, 6));
-  colors.push(color(237, 70, 47, 1));
-  //colors.push(color(123, 123, 98, alph));
-  // colors.push(color(64, 64, 64, alph));  
-  smallPoint = 20;
-  largePoint = 60;
-  imageMode(CENTER);
-  noStroke();
-  clear();
-  angleMode(RADIANS);
+  
+
+  background(255);
+
+
 }
 
 function draw() {
 
-  for (var i = 0; i < 15; i++) {
-    var v = p5.Vector.random2D();
-
-    var wave = map(sin(angle), -1, 1, 0, 4);
-
-    v.mult(random(1, 20*wave));
-    var pointillize = random(smallPoint, largePoint);
-    var x = mouseX + v.x;//floor(random(img.width));
-    var y = mouseY + v.y;//floor(random(img.height));
-    //var pix = img.get(x, y);
-    //fill(pix[0],pix[1], pix[2], 52);
-    fill(colors[index]);
-    ellipse(x, y, pointillize, pointillize);
+  for (let walk of walks) {
+  walk.step();
+  walk.display();
   }
 
-  if (random(1) < 0.01) {
-    index = (index + 1) % colors.length;
-  }
-
-  angle += 0.02;
+    
 }
+
+
+
+function mousePressed() {
+  // let r = random(10, 50);
+  let w = new Walker(mouseX, mouseY);
+  walks.push(w);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
