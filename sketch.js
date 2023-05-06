34c6e117a95f59a988e363fd262fcacea92f5ee9
manbir87 <38939907+kewlfool@users.noreply.github.com>
2023-05-06 09:14:46 -0300

    new learning project, snowfall

--- sketch.js
+++ sketch.js
@@ -1,64 +1,52 @@
 
 
-let pos;
-
-
-let walks = [];
+var snow = [];
 
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+
+
 function setup() {
-  
-   var canvas = createCanvas(windowWidth, windowHeight);
-  canvas.id('sketch-container'); 
-  
-
+  createCanvas(windowWidth, windowHeight);
   background(255);
-
+  // frameRate(1);  // Animate slowly
+  snow.push(new Snowflake());
 
 }
 
 function draw() {
+  background(51);
+  // Draws the snowflake!
+  k.render();
+  // Iterate
 
-  for (let walk of walks) {
-  walk.step();
-  walk.display();
+  rect(mouseX, mousey, 50, 50);
+
+  // Let's not do it more than 5 times. . .
+  if (k.getCount() > 7) {
+    k.restart();
   }
 
-    
+
+
+}
+
+function keyPressed() {
+
+  if (key == ' ') {
+
+    k.nextLevel();
+
+  }
+
 }
 
 
+function mouseMoved() {
 
-function mousePressed() {
-  // let r = random(10, 50);
-  let w = new Walker(mouseX, mouseY);
-  walks.push(w);
+  k.moves();
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
