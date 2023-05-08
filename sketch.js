60aeca34565a9892344d0805e15b7acdb220b291
manbir87 <38939907+kewlfool@users.noreply.github.com>
2023-05-08 20:20:32 -0300

    walker

--- sketch.js
+++ sketch.js
@@ -1,91 +1,64 @@
 
 
-let snow = [];
+let pos;
 
-let gravity;
-// let wind;
 
-let file;
-let textures = [];
-let design;
-let xoff = 0;
+let walks = [];
 
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function preload() {
-  file = loadImage('media/flakes32.png')
-}
-
-
-
-
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // background(25);
-  gravity = createVector(0, 0.03);
-  // wind = createVector(random(1), 0);
+
+  var canvas = createCanvas(windowWidth, windowHeight);
+  canvas.id('sketch-container');
 
 
-  for (let x = 0; x < file.width; x += 32) {
-    for (let y = 0; y < file.height; y += 32) {
-
-      let img = file.get(x, y, 32, 32);
-
-      textures.push(img);
-
-    }
-
-  }
-
-
-
-  // snow.push(new Snowflake(design));
-
-
-
+  background(205);
 
 
 }
-
-
-
 
 function draw() {
-  background(10);
-  // image(file, 0, 0);
 
-
-  let wAngle = noise(xoff) * TWO_PI;
-
-  // let wind = p5.Vector.fromAngle(wAngle);
-  // xoff += 0.1;
-
-  let wx = map(mouseX, 0, width, -0.02, 0.02);
-  wind = createVector(wx, 0);
-
-
-  design = random(textures);
-  snow.push(new Snowflake(design));
-
-  // Draws the snowflake!
-  for (flake of snow) {
-    flake.applyForce(gravity);
-    flake.applyForce(wind);
-    flake.render();
-    flake.update();
+  for (let walk of walks) {
+    walk.step();
+    walk.display();
   }
-  // Iterate
 
-  for (let i = snow.length - 1; i >= 0; i--) {
-    if (snow[i].offScreen()) {
 
-      snow.splice(i, 1);
-    }
-  }
 }
+
+
+
+function mousePressed() {
+  // let r = random(10, 50);
+  let w = new Walker(mouseX, mouseY);
+  walks.push(w);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
