c88afe9b31cf14bf74c09e505042a6112921d0d7
manbir87 <38939907+kewlfool@users.noreply.github.com>
2023-05-28 10:26:25 -0300

    site update

--- walker.js
+++ walker.js
@@ -1,9 +1,9 @@
 class Walker {
-  constructor(x = width /2, y = height / 2) {
-    this.pos = createVector(x,y);
-    this.vel  = createVector(4,4);
-    this.c =  color(random(255),random(255),random(255),50);
-   }
+  constructor(x = width / 2, y = height / 2) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(4, 4);
+    this.c = color(random(255), random(255), random(255), 50);
+  }
 
   display() {
     strokeWeight(5);
@@ -12,31 +12,31 @@ class Walker {
   }
 
   step() {
-    
-    
-    
-    
-    
+
+
+
+
+
     let r = floor(random(4));
 
     switch (r) {
       case 0:
-        
-      this.pos.x += this.vel.x;
+
+        this.pos.x += this.vel.x;
         break;
 
       case 1:
-      
+
         this.pos.x -= this.vel.x;
         break;
-        
+
       case 2:
-     
+
         this.pos.y += this.vel.y;
         break;
-        
+
       case 3:
-      
+
         this.pos.y -= this.vel.y;
         break;
     }
